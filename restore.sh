79a397ee7ee3199e69ca6aada54f6e57d2a1ff49
a.jacobs <a.jacobs@nexxit.be>
2024-11-07 15:47:47 +0100

    Improve database creation logging in restore script
    Add messages to log actions related to database existence checks and creation. This provides better clarity on the script's execution flow, especially when a database does not exist.

--- restore.sh
+++ restore.sh
@@ -229,9 +229,11 @@ if [ "$db" != "" ]; then
   mysql -e "CREATE DATABASE \`$bck_db_name\`;"
   mysql "$db_name" -sNe 'show tables' | while read table; do mysql -sNe "RENAME TABLE \`$db_name\`.$table TO \`$bck_db_name\`.$table"; done
 	mysql -e "DROP DATABASE \`$db_name\`;"
+else
+	echo "Database ${db_name} does not exists"
 fi
-
 # Create/Recreate database
+echo "Create or Recreate Database ${db_name}"
 mysql -e "CREATE DATABASE \`$db_name\`;"
 
 # Restore database from file
